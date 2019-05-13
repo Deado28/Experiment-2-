d01dfba0fe05d74673f1396e885616460455d7cb
Deado <47809722+Deado28@users.noreply.github.com>
2019-05-13 23:19:04 +0800

    Add files via upload

---
+++ Experiment2-6.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <conio.h>
+#include <iomanip>
+using namespace std;
+
+int main ()
+{
+	int a, b, c = 0;
+
+	do
+	{ 
+		cout << "Enter a number: ";
+		cin >> a;
+
+		if (a <= 0)
+			cout << "Thank you!\n";
+		else
+		{
+			for (b = 1; b <= a; b++)
+			{
+				c += b;
+			}
+			cout << "The Sum of All Whole Numbers From 1 to " << a << " is "<< c << "\n";
+			c = 0;
+		}
+	}
+	while (a != 0);
+
+	_getch();
+	return 0;
+}
+
